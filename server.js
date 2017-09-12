f551df3f694260006bbb62e82d1679ca6ff30065
Expeditions-Team <expeditions.game.team@gmail.com>
2017-09-12 22:00:37 +0200

    Serwer v2

--- server.js
+++ server.js
@@ -1,60 +1,135 @@
-var http = require('http'),
-	PORT = 8080,
-	qs = require('querystring'),
-	fs = require('fs'),
-	path = require('path'),
-	mime = require('mime'),
-	cache = {};
+//  OpenShift sample Node application
+var	express = require('express'),
+		app     = express(),
+		morgan  = require('morgan');
+    
+Object.assign=require('object-assign')
 
-function send404(response)
-{
-	//response.setEncoding('utf-8');
-	response.writeHead(404, {'Content-Type': 'text/plain'});
-	response.write('Błąd 404: Nie znaleziono strony o podanym adresie :(');
-	response.end();
-}
+app.engine('html', require('ejs').renderFile);
+app.use(morgan('combined'))
 
-function sendFile(response, filePath, fileContents)
-{
-	response.writeHead(200, {"content-type": mime.lookup(path.basename(filePath)) });
-	response.end(fileContents);
-}
+var	port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+		ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+		mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+		mongoURLLabel = "";
 
-function serveStatic(response, cache, absPath)
+if (mongoURL == null && process.env.DATABASE_SERVICE_NAME)
 {
-	if (cache[absPath])
+	var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+	mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+	mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+	mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+	mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+	mongoUser = process.env[mongoServiceName + '_USER'];
+
+	if (mongoHost && mongoPort && mongoDatabase)
 	{
-		console.log("Ktoś próbuje się połączyć: "+ cache[absPath]);
-		sendFile(response, absPath, cache[absPath]);
+		mongoURLLabel = mongoURL = 'mongodb://';
+		
+		if (mongoUser && mongoPassword)
+		{
+			mongoURL += mongoUser + ':' + mongoPassword + '@';
+		}
+		
+		// Provide UI label that excludes user id and pw
+		mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+		mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+	}
+}
+
+var	db = null,
+		dbDetails = new Object();
+
+var initDb = function(callback)
+{
+	if (mongoURL == null) 		return;
+
+	var mongodb = require('mongodb');
+	
+	if (mongodb == null)			 return;
+
+	mongodb.connect(mongoURL, function(err, conn)
+	{
+		if (err)
+		{
+			callback(err);
+			return;
+		}
+
+		db = conn;
+		dbDetails.databaseName = db.databaseName;
+		dbDetails.url = mongoURLLabel;
+		dbDetails.type = 'MongoDB';
+
+		console.log('Connected to MongoDB at: %s', mongoURL);
+	});
+};
+
+app.get('/', function (req, res)
+{
+	// try to initialize the db on every request if it's not already
+	// initialized.
+	if (!db) 
+	{
+		initDb(function(err){});
+	}
+	
+	if (db)
+	{
+		var col = db.collection('counts');
+		// Create a document with request IP and current time of request
+		col.insert({ip: req.ip, date: Date.now()});
+		col.count(function(err, count)
+		{
+			res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+		});
+	} 
+	else
+	{
+		res.render('index.html', { pageCountMessage : null});
+	}
+});
+
+app.get('/pagecount', function (req, res)
+{
+	// try to initialize the db on every request if it's not already
+	// initialized.
+	if (!db)
+	{
+		initDb(function(err){});
+	}
+
+	if (db)
+	{
+		db.collection('counts').count(function(err, count )
+		{
+			res.send('{ pageCount: ' + count + '}');
+		});
 	}
 	else
 	{
-		fs.exists(absPath, function(exists)
-		{
-			if (exists)
-			{
-				fs.readFile(absPath, function(err, data)
-				{
-					if (err)
-					{
-						send404(response);
-					}
-					else
-					{
-						cache[absPath] = data;
-						sendFile(response, absPath, data);
-					}
-				});
-			}
-			else
-			{
-				send404(response);
-			}
-		});
+		res.send('{ pageCount: -1 }');
 	}
-}
+});
 
-var server = http.createServer(function(request, response)
+// error handling
+app.use(function(err, req, res, next)
+{
+	console.error(err.stack);
+	res.status(500).send('Something bad happened!');
+});
+
+initDb(function(err)
+{
+	console.log('Error connecting to Mongo. Message:\n'+err);
+});
+
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+module.exports = app;
+
+/*var server = http.createServer(function(request, response)
 {
 	var filePath = false;
 
@@ -78,7 +153,7 @@ var server = http.createServer(function(request, response)
 	var absPath = './' + filePath;
 	
 	serveStatic(response, cache, absPath);
-});
+});*/
 
 server.listen(PORT, 'localhost', function()
 {
