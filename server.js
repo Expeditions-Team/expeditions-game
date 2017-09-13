156904af808af5551f5e4503366366b19c205d19
Expeditions-Team <expeditions.game.team@gmail.com>
2017-09-13 15:57:10 +0200

    Serwer v2

--- server.js
+++ server.js
@@ -119,32 +119,6 @@ app.get('/game', function (req, res)
 	console.log("Ktoś chciał się połączyć z \""+ req.url +"\"");
 });
 
-app.get('/jQuery.js', function (req, res)
-{
-	// try to initialize the db on every request if it's not already
-	// initialized.
-	if (!db) 
-	{
-		initDb(function(err){});
-	}
-	
-	if (db)
-	{
-		var col = db.collection('counts');
-		// Create a document with request IP and current time of request
-		col.insert({ip: req.ip, date: Date.now()});
-		col.count(function(err, count)
-		{
-			res.render('jQuery.js', { pageCountMessage : count, dbInfo: dbDetails });
-		});
-	} 
-	else
-	{
-		res.render('jQuery.js', { pageCountMessage : null});
-	}
-	console.log("Ktoś chciał się połączyć z \""+ req.url +"\"");
-});
-
 app.get('/pagecount', function (req, res)
 {
 	// try to initialize the db on every request if it's not already
