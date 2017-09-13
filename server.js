7e1d9be911babdf49626f9016bba2bacb0d234c5
Expeditions-Team <expeditions.game.team@gmail.com>
2017-09-13 15:07:51 +0200

    Serwer v2

--- server.js
+++ server.js
@@ -92,6 +92,31 @@ app.get('/', function (req, res)
 	}
 });
 
+app.get('/game', function (req, res)
+{
+	// try to initialize the db on every request if it's not already
+	// initialized.
+	if (!db) 
+	{
+		initDb(function(err){});
+	}
+	
+	if (db)
+	{
+		var col = db.collection('counts');
+		// Create a document with request IP and current time of request
+		col.insert({ip: req.ip, date: Date.now()});
+		col.count(function(err, count)
+		{
+			res.render('game.html', { pageCountMessage : count, dbInfo: dbDetails });
+		});
+	} 
+	else
+	{
+		res.render('game.html', { pageCountMessage : null});
+	}
+});
+
 app.get('/pagecount', function (req, res)
 {
 	// try to initialize the db on every request if it's not already
