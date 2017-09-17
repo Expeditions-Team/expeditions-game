c8aac67a102850fb28f121e0cbd08c9eae130cd5
Expeditions-Team <expeditions.game.team@gmail.com>
2017-09-18 01:42:40 +0200

    Serwer v0.0.5

--- public/JavaScripts/version.js
+++ public/JavaScripts/version.js
@@ -1,3 +1,4 @@
-var version = "v0.0.4", description = "Naprawianie chodzenia (Skraje mapy)";
-$("#life_bar").html(version);
-$("#mana_bar").html(description);
+var version = "v0.0.5", description = "Działające chodzenie, do zrobienia przełączanie się między kontami oraz usuwanie gracza z tablicy, gdy go nie widać (z marginesem poza widocznym ekranem) i przy wylogowaniu/wyłączeniu karty przeglądarki";
+
+$("#messages").append( $('<li class="message">').text("GLaDOS "+ version +': '+ description) );
+$("#messages").scrollTop( $("#messages").height() );
